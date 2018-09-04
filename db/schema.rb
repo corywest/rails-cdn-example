01fc48f9d573f3de22b7897b3a8ad261eac29720
Cory Westerfield <cory.westerfield@gmail.com>
2018-09-04 17:25:57 -0600

    Add db config

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 20180820192741) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "content"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_comments_on_user_id"
@@ -28,4 +31,5 @@ ActiveRecord::Schema.define(version: 20180820192741) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users"
 end
